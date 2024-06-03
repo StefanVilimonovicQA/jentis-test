b94cb8a31ae9c639c061bac490b8a761a9f635e5
Stefan Vilimonovic <vilimonovic@outlook.com>
2024-06-03 18:08:38 +0200

    Delete user negative cases

--- cypress/e2e/api/users/deleteUser.cy.ts
+++ cypress/e2e/api/users/deleteUser.cy.ts
@@ -1,14 +1,16 @@
 import { createRandomUser } from '../../../fixtures/createUser';
+import { faker } from '@faker-js/faker';
 
 describe('Contacts API tests', () => {
     it('Delete user', () => {
         const user = createRandomUser()
         cy.addUser(user.firstName, user.lastName, user.email, user.password).then((response) => {
+            console.log(response)
             cy.request({
                 method: 'DELETE',
                 url: 'https://thinking-tester-contact-list.herokuapp.com/users/me',
                 auth: {
-                    bearer: response.token
+                    bearer: response.body.token
                   }
               }).then((result) => {
                 expect(result.status).to.eq(200);
@@ -16,4 +18,34 @@ describe('Contacts API tests', () => {
         })
         
     })
+    it('Delete user with empty authorization token', () => {
+            cy.request({
+                method: 'DELETE',
+                url: 'https://thinking-tester-contact-list.herokuapp.com/users/me',
+                failOnStatusCode: false,
+                auth: {
+                    bearer: ""
+                  }
+              }).then((result) => {
+                expect(result.status).to.eq(401);
+                expect(result.body.error).to.eq("Please authenticate.")
+              })
+        
+        
+    })
+    it('Delete user with invalid authorization token', () => {
+        cy.request({
+            method: 'DELETE',
+            url: 'https://thinking-tester-contact-list.herokuapp.com/users/me',
+            failOnStatusCode: false,
+            auth: {
+                bearer: faker.string.uuid()
+              }
+          }).then((result) => {
+            expect(result.status).to.eq(401);
+            expect(result.body.error).to.eq("Please authenticate.")
+          })
+    
+    
+})
 })
