7849d535515cb5619cbdaaa231b7cd4b66bbbbce
Stefan Vilimonovic <vilimonovic@outlook.com>
2024-06-10 20:56:56 +0200

    login UI tests

---
+++ cypress/e2e/ui/login.cy.ts
@@ -0,0 +1,75 @@
+import { faker } from '@faker-js/faker';
+describe('Login UI tests', () => {
+    const username = 'test@ui.com'
+    const password = 'test123'
+  beforeEach(() => {
+    cy.visit('/').get('h1').should('have.text', 'Contact List App');
+    cy.intercept('/users/login').as('loginAPI');
+  });
+  it('Successful login', () => {
+    cy.get('#email')
+      .should('have.attr', 'placeholder', 'Email')
+      .type(username);
+    cy.get('#password')
+      .should('have.attr', 'placeholder', 'Password')
+      .type(password);
+    cy.get('button#submit').click();
+    cy.wait('@loginAPI');
+    cy.get('h1').should('have.text', 'Contact List');
+  });
+  it('Login with invalid credentials', () => {
+    const invalidCredentials = [
+      { email: faker.internet.email(), password: faker.string.uuid() },
+      { email: faker.internet.email(), password: password },
+      { email: username, password: faker.string.uuid() },
+    ];
+
+    invalidCredentials.forEach((cred) => {
+        cy.log(`***Email value: ${cred.email}***`)
+        cy.log(`***Password value: ${cred.password}***`)
+      cy.get('#email').clear()
+        .should('have.attr', 'placeholder', 'Email')
+        .type(cred.email);
+      cy.get('#password').clear()
+        .should('have.attr', 'placeholder', 'Password')
+        .type(cred.password);
+      cy.get('button#submit').click();
+
+      cy.wait('@loginAPI').then((xhr) => {
+        expect(xhr.response.statusCode).to.eq(401);
+      });
+      cy.get('span#error')
+        .should('be.visible')
+        .and('exist')
+        .and('have.text', 'Incorrect username or password');
+    });
+  });
+  it('Login with empty credentials', () => {
+    const emptyCredentials = [
+        { email: ' ', password: ' ' },
+        { email: ' ', password: password },
+        { email: username, password: ' ' },
+      ];
+  
+      emptyCredentials.forEach((cred) => {
+        cy.log(`***Email value: ${cred.email}***`)
+        cy.log(`***Password value: ${cred.password}***`)
+
+        cy.get('#email').clear()
+          .should('have.attr', 'placeholder', 'Email')
+          .type(cred.email);
+        cy.get('#password').clear()
+          .should('have.attr', 'placeholder', 'Password')
+          .type(cred.password);
+        cy.get('button#submit').click();
+  
+        cy.wait('@loginAPI').then((xhr) => {
+          expect(xhr.response.statusCode).to.eq(401);
+        });
+        cy.get('span#error')
+          .should('be.visible')
+          .and('exist')
+          .and('have.text', 'Incorrect username or password');
+      });
+  });
+});
